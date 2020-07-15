8bd48306ed4c4d04bf385565d44f1ee1e94e485d
huangyanzu <709245577@qq.com>
2020-07-15 16:25:01 +0800

    初始化项目

---
+++ SecondApp/SecondApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondApp
+//
+//  Created by 彦祖 on 2020/7/15.
+//  Copyright © 2020 彦祖. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
